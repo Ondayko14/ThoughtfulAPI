e2cb246c22846f18384ddb69f1402c25018b87b9
Ondayko14 <brandon@carmigo.io>
2021-02-21 12:01:41 -0600

    thoughts can now be updated

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -81,6 +81,19 @@ const thoughtController = {
         res.status(400).json(err);
     })
 },
+
+//Update a thought
+updateThought({params, body}, res) {
+  Thought.findOneAndUpdate({_id: params.id}, body, {new: true})
+  .then(dbThoughtData => {
+      if (!dbThoughtData) {
+          res.status(404).json({message: "no thought found with this id"});
+          return;
+      }
+      res.json(dbThoughtData);
+  })
+  .catch(err => res.status(400).json(err));
+},
 };
 
 module.exports = thoughtController;
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { addThought, removeThought, getThoughts, getThoughtById } = require('../../controllers/thought-controller');
+const { addThought, removeThought, getThoughts, getThoughtById, updateThought } = require('../../controllers/thought-controller');
 
 router.route('/:userId').post(addThought);
 
@@ -9,4 +9,5 @@ router.route('/').get(getThoughts);
 
 router.route('/:id').get(getThoughtById);
 
+router.route('/:id').put(updateThought);
 module.exports = router;
